4f8eb482923390717a73d308390ef5e176379a9b
Vishnu Priyan S S <vishnu32510@gmail.com>
2024-02-17 12:07:57 -0600

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Vishnu Priyan Sellam Shanmugavel on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
